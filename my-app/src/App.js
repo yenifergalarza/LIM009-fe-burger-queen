709fdd8b16fe719ac01594641ba335f5423584ad
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-07-11 17:30:55 -0500

    maquetando header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/burgerqueen.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="App-text">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
