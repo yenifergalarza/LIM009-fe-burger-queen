9c55f10f9737f5484bf3d3167703c93d8cb77101
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-07-12 17:29:27 -0500

    pagina con header y conteo de productos falta estructurar css y vistas

---
+++ my-app/src/components/product.js
@@ -0,0 +1,38 @@
+import React from 'react';
+const Product =({title,price,inventory,addToCart,removeFromCart,deleteFromCart})=>(
+ <div style={{marginBotton:'1.5rem'}}>
+   <p><strong>{title}</strong>${price}</p>
+ <div className="field is-grouped">
+ <p className="control">
+ <span className = "tag is-warning is-medium">{inventory}</span>
+    
+   </p>
+
+   <p className="control">
+     <a className="button is-link" onClick={addToCart}>
+     <span className="icon is-small">
+<i className="fas fa-plus"></i>
+</span>
+ 
+      </a>
+   </p>
+   <p className="control">
+     <a className="button" onClick={removeFromCart}>
+     <span className="icon is-small">
+<i className="fas fa-minus"></i>
+</span>
+    </a>
+   </p>
+   <p className="control">
+     <a className="button is-danger" onClick={deleteFromCart}>
+     <span className="icon is-small">
+<i className="fas fa-trash"></i>
+</span>
+      </a>
+   </p>
+ </div>
+ </div>
+    
+);
+
+export default Product;
