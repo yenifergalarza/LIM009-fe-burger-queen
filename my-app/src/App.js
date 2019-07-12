79ccf7dac9af8f7c5f2b6e832587467028e81105
Maga <magabriela.lops@gmail.com>
2019-07-11 21:04:03 -0500

    first steps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './assets/burgerqueen.png';
 import './App.css';
+import {Header, Section} from './components'
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-text">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Header text="Hello burguerQueen" />
+      <Section/>
+      </div>
   );
 }
 
