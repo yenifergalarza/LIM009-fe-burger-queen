432752c4d853fdcb96ab74b4ea12b8f896a8bcab
Maga <magabriela.lops@gmail.com>
2019-07-15 17:55:49 -0500

    config eslint and corrections in code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,105 +1,115 @@
-import React, { Component, Fragment } from 'react';
-import NavBar from './components/navBar';
-import ProductList from './components/productList';
-
+import React, { Component, Fragment } from "react";
+import NavBar from "./components/navBar";
+import ProductList from "./components/productList";
 //import './App.css';
+
 const initialProducts = [
-  { id: 1, title: "hamburguesa simple de pollo", inventory: 0, price: 10.00 }, 
-  { id: 2, title: "hamburguesa simple vegana", inventory: 0, price: 10.00 },
-  { id: 3, title: "hamburguesa simple de carne", inventory: 0, price: 10.00 },
-  { id: 4, title: "hamburguesa doble de pollo", inventory: 0, price: 15.00 }, 
-  { id: 5, title: "hamburguesa doble vegana", inventory: 0, price: 15.00 },
-  { id: 6, title: "hamburguesa doble de carne", inventory: 0, price: 15.00 },
-  { id: 7, title: "Papas fritas", inventory: 0, price: 5.00 },
-  { id: 8, title: "Aros de cebolla", inventory: 0, price: 5.00 },
-  { id: 9, title: "Agua 500ml", inventory: 0, price: 5.00 },
-  { id: 10, title: "Agua 750ml", inventory: 0, price: 7.00 },
-  { id: 11, title: "Bebida/gaseosa 500ml", inventory: 0, price: 7.00 },
-  { id: 12, title: "Bebida/gaseosa 750ml", inventory: 0, price: 10.00 },
-  { id: 13, title: "Queso Extra", inventory: 0, price: 1.00 },
-  { id: 14, title: "Huevo Extra", inventory: 0, price: 1.00 },
-  { id: 15, title: "Café con leche", inventory: 0, price: 7.00 },
-  { id: 16, title: "Café americano", inventory: 0, price: 5.00 },
-  { id: 13, title: "Sandwich de jamón y queso", inventory: 0, price: 10.00 },
-  { id: 14, title: "Jugo de frutas natural", inventory: 0, price: 7.00 },
+  { id: 1, title: "hamburguesa simple de pollo", inventory: 0, price: 10.0 },
+  { id: 2, title: "hamburguesa simple vegana", inventory: 0, price: 10.0 },
+  { id: 3, title: "hamburguesa simple de carne", inventory: 0, price: 10.0 },
+  { id: 4, title: "hamburguesa doble de pollo", inventory: 0, price: 15.0 },
+  { id: 5, title: "hamburguesa doble vegana", inventory: 0, price: 15.0 },
+  { id: 6, title: "hamburguesa doble de carne", inventory: 0, price: 15.0 },
+  { id: 7, title: "Papas fritas", inventory: 0, price: 5.0 },
+  { id: 8, title: "Aros de cebolla", inventory: 0, price: 5.0 },
+  { id: 9, title: "Agua 500ml", inventory: 0, price: 5.0 },
+  { id: 10, title: "Agua 750ml", inventory: 0, price: 7.0 },
+  { id: 11, title: "Bebida/gaseosa 500ml", inventory: 0, price: 7.0 },
+  { id: 12, title: "Bebida/gaseosa 750ml", inventory: 0, price: 10.0 },
+  { id: 13, title: "Queso Extra", inventory: 0, price: 1.0 },
+  { id: 14, title: "Huevo Extra", inventory: 0, price: 1.0 },
+  { id: 15, title: "Café con leche", inventory: 0, price: 7.0 },
+  { id: 16, title: "Café americano", inventory: 0, price: 5.0 },
+  { id: 13, title: "Sandwich de jamón y queso", inventory: 0, price: 10.0 },
+  { id: 14, title: "Jugo de frutas natural", inventory: 0, price: 7.0 }
 ];
-const formatNumber = (number) => new Intl.NumberFormat('en-US',{mininumFractionDigits:2,maximunFractionDigits:2}).format(number);
+const formatNumber = number =>
+  new Intl.NumberFormat("en-US", {
+    mininumFractionDigits: 2,
+    maximunFractionDigits: 2
+  }).format(number);
+
 class App extends Component {
- 
-  state={
-    addedIds :[],
-    quantityById:{},
-    products:initialProducts,
+  state = {
+    addedIds: [],
+    quantityById: {},
+    products: initialProducts
   };
-  addToCart = (id) =>{
-const {addedIds,quantityById,products} = this.state;
-const product = products.find(prod => prod.id === id);
-const available = 1;
-if (available > 0){
-  const newAddedIds = addedIds.find(prodId => prodId === id) ? addedIds : addedIds.concat(product.id);
-  const newQuantityById ={
-    ...quantityById,
-    [id]:(quantityById[id]|| 0) + 1,
-  };  this.setState({ addedIds :newAddedIds,quantityById :newQuantityById});
-}
-  };
-  removeFromCart = (id) =>{
-const { addedIds,quantityById}=this.state;
-if(quantityById[id] ){
-  const newQuantityById ={
-    ...quantityById,
-    [id]:quantityById[id]>1 ? quantityById[id] -1:undefined,
-
-  };
-  const newAddedIds = newQuantityById[id]?addedIds :addedIds.filter(prodId =>prodId !== id);
-  this.setState({addedIds :newAddedIds,quantityById :newQuantityById});
-};
-  };
-  deleteFromCart = (id)=>{
-    const { addedIds,quantityById}=this.state;
-    if(quantityById[id] ){
-      const newQuantityById ={
+  addToCart = id => {
+    const { addedIds, quantityById, products } = this.state;
+    const product = products.find(prod => prod.id === id);
+    const available = 1;
+    if (available > 0) {
+      const newAddedIds = addedIds.find(prodId => prodId === id)
+        ? addedIds
+        : addedIds.concat(product.id);
+      const newQuantityById = {
         ...quantityById,
-        [id]:undefined
+        [id]: (quantityById[id] || 0) + 1
       };
-      const newAddedIds = addedIds.filter(prodId => prodId !== id); 
-      this.setState({addedIds :newAddedIds,quantityById :newQuantityById});
-    };
-
+      this.setState({ addedIds: newAddedIds, quantityById: newQuantityById });
+    }
   };
-  getAvailable = (products,quantityById) =>{
-return products.reduce(
-  (res,product)=>({
-    ...res,
-  [ product.id]:quantityById[product.id]||0,
-  }),
-  {}
-);
+  removeFromCart = id => {
+    const { addedIds, quantityById } = this.state;
+    if (quantityById[id]) {
+      const newQuantityById = {
+        ...quantityById,
+        [id]: quantityById[id] > 1 ? quantityById[id] - 1 : undefined
+      };
+      const newAddedIds = newQuantityById[id]
+        ? addedIds
+        : addedIds.filter(prodId => prodId !== id);
+      this.setState({ addedIds: newAddedIds, quantityById: newQuantityById });
+    }
   };
-  getTotal = (products,addedIds,quantityById) => {
+  deleteFromCart = id => {
+    const { addedIds, quantityById } = this.state;
+    if (quantityById[id]) {
+      const newQuantityById = {
+        ...quantityById,
+        [id]: undefined
+      };
+      const newAddedIds = addedIds.filter(prodId => prodId !== id);
+      this.setState({ addedIds: newAddedIds, quantityById: newQuantityById });
+    }
+  };
+  getAvailable = (products, quantityById) => {
+    return products.reduce(
+      (res, product) => ({
+        ...res,
+        [product.id]: quantityById[product.id] || 0
+      }),
+      {}
+    );
+  };
+  getTotal = (products, addedIds, quantityById) => {
     return addedIds.reduce(
-      (res,productId)=>res + products.find(prod =>prod.id === productId ).price*(quantityById[productId]|| 0),
+      (res, productId) =>
+        res +
+        products.find(prod => prod.id === productId).price *
+          (quantityById[productId] || 0),
       0
     );
-
   };
 
-  render()
-  {
-    const {products,quantityById,addedIds}= this.state
-    const available = this.getAvailable(products,quantityById)
-  const total = this.getTotal(products,addedIds,quantityById)
-  return (<Fragment>
-    <NavBar total={formatNumber(total)} />
-   <ProductList 
-   available = {available}
-   products={initialProducts} 
-    addToCart={this.addToCart}
-    removeFromCart ={this.removeFromCart}
-    deleteFromCart = {this.deleteFromCart}
-    />
-  </Fragment>
-  );
-}}
+  render() {
+    const { products, quantityById, addedIds } = this.state;
+    const available = this.getAvailable(products, quantityById);
+    const total = this.getTotal(products, addedIds, quantityById);
+    return (
+      <Fragment>
+        <NavBar total={formatNumber(total)} />
+        <ProductList
+          available={available}
+          products={initialProducts}
+          addToCart={this.addToCart}
+          removeFromCart={this.removeFromCart}
+          deleteFromCart={this.deleteFromCart}
+        />
+      </Fragment>
+    );
+  }
+}
 
 export default App;
--- my-app/src/components/productList.js
+++ my-app/src/components/productList.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import Product from './product'
+import React from "react";
+import Product from "./product";
 
-const ProductList = ({products,addToCart,removeFromCart,deleteFromCart,available})=>(
+const ProductList = ({
+  products,
+  addToCart,
+  removeFromCart,
+  deleteFromCart,
+  available
+}) => {
+  return (
     <div className="container">
-    <div clasname="column is-centered">
-      <div clasname="column is-narrow">
-        {products.map(prod =>    
-        <Product
-        key={prod.id}
-        title={prod.title}
-        inventory={available[prod.id]}
-         price={prod.price}  
-    addToCart ={()=>addToCart(prod.id)}
-    removeFromCart = {()=>removeFromCart(prod.id)}
-    deleteFromCart = {()=>deleteFromCart(prod.id)}
-    />)}
+      <div clasname="column is-centered">
+        <div clasname="column is-narrow">
+          {products.map(prod => (
+            <Product
+              key={prod.id}
+              title={prod.title}
+              inventory={available[prod.id]}
+              price={prod.price}
+              addToCart={() => addToCart(prod.id)}
+              removeFromCart={() => removeFromCart(prod.id)}
+              deleteFromCart={() => deleteFromCart(prod.id)}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
 export default ProductList;
