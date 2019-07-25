92ef35fa243746d46e2bf85b2bb13c2310325b2a
Maga <magabriela.lops@gmail.com>
2019-07-25 16:54:04 -0500

    commitagain

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
-import React, {  Fragment,useState,setState} from "react";
+import React, { Fragment, useState, setState } from "react";
 import NavBar from "./components/navBar.jsx";
 import ProductList from "./components/productList.jsx";
 import "./App.css";
 import ContainerMenu from "./components/ContainerMenu.jsx";
 
-import dataProducts from './data';
-
+import dataProducts from "./data";
 
 const formatNumber = number =>
   new Intl.NumberFormat("en-US", {
@@ -13,22 +12,28 @@ const formatNumber = number =>
     maximunFractionDigits: 2
   }).format(number);
 
-      const newAddedIds = addedIds.find(prodId => prodId === id)
-        ? addedIds
-        : addedIds.concat(product.id);
-      const newQuantityById = {
-        ...quantityById,
-        [id]: (quantityById[id] || 0) + 1
-      };    
-       setQuantityById(newQuantityById);
-      setAddedIds(newAddedIds) ;
-     return [addedIds,quantityById]
-    
+const App = () => {
+  const [addedIds, setAddedIds] = useState([]);
+  const [quantityById, setQuantityById] = useState({});
+  const [products] = useState(dataProducts);
+
+  const addToCart = id => {
+    const product = products.find(prod => prod.id === id);
+
+    const newAddedIds = addedIds.find(prodId => prodId === id)
+      ? addedIds
+      : addedIds.concat(product.id);
+    const newQuantityById = {
+      ...quantityById,
+      [id]: (quantityById[id] || 0) + 1
+    };
+    setQuantityById(newQuantityById);
+    setAddedIds(newAddedIds);
+
+    return [addedIds, quantityById];
   };
 
-
   const removeFromCart = id => {
-  
     if (quantityById[id]) {
       const newQuantityById = {
         ...quantityById,
@@ -37,13 +42,12 @@ const formatNumber = number =>
       const newAddedIds = newQuantityById[id]
         ? addedIds
         : addedIds.filter(prodId => prodId !== id);
-        setAddedIds(newAddedIds);
-        setQuantityById(newQuantityById);
+      setAddedIds(newAddedIds);
+      setQuantityById(newQuantityById);
     }
-    return [addedIds,quantityById]
+    return [addedIds, quantityById];
   };
   const deleteFromCart = id => {
-
     if (quantityById[id]) {
       const newQuantityById = {
         ...quantityById,
@@ -53,7 +57,7 @@ const formatNumber = number =>
       setAddedIds(newAddedIds);
       setQuantityById(newQuantityById);
     }
-    return [addedIds,quantityById]
+    return [addedIds, quantityById];
   };
   const getAvailable = (products, quantityById) => {
     return products.reduce(
@@ -64,7 +68,7 @@ const formatNumber = number =>
       {}
     );
   };
- const getTotal = (products, addedIds, quantityById) => {
+  const getTotal = (products, addedIds, quantityById) => {
     return addedIds.reduce(
       (res, productId) =>
         res +
@@ -74,30 +78,25 @@ const formatNumber = number =>
     );
   };
 
-  
-    // const { products, quantityById, addedIds } = state;
-    const available = getAvailable(products, quantityById);
-    const total = getTotal(products, addedIds, quantityById);
-    return (
-      <Fragment>
-        <NavBar total={formatNumber(total)} />
-        <section className="tile is-ancestor  pd-prodlist is-12 spaceEvenly">
-         <ContainerMenu>
+  // const { products, quantityById, addedIds } = state;
+  const available = getAvailable(products, quantityById);
+  const total = getTotal(products, addedIds, quantityById);
+  return (
+    <Fragment>
+      <NavBar total={formatNumber(total)} />
+      <section className="tile is-ancestor  pd-prodlist is-12 spaceEvenly">
+        <ContainerMenu></ContainerMenu>
 
-         </ContainerMenu>
-
-          <ProductList
-            available={available}
-            products={dataProducts}
-            addToCart={addToCart}
-            removeFromCart={removeFromCart}
-            deleteFromCart={deleteFromCart}
-          />
-        </section>
-      </Fragment>
-    );
-  
-  
-}
+        <ProductList
+          available={available}
+          products={dataProducts}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+          deleteFromCart={deleteFromCart}
+        />
+      </section>
+    </Fragment>
+  );
+};
 
 export default App;
