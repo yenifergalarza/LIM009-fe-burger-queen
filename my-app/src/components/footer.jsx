29fc80e071e3200bb608d4e6a9d58cf755965339
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-08-12 00:00:30 -0500

    estilos footer

--- my-app/src/components/footer.jsx
+++ my-app/src/components/footer.jsx
@@ -5,9 +5,7 @@ const  Footer= () =>{
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          <strong>Burguer - Queen</strong> por  <a href="https://github.com/magalopez">Maria Lopéz</a> y <a href="https://github.com/yenifergalarza">Yenifer Galarza</a>.
         </p>
       </div>
     </div>
