90e30ca138ab56f426f042d8dfd17c461061f73a
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-08-19 13:49:09 -0500

    Updates

---
+++ precache-manifest.e96f38d16d819fd3d06a659e4dd21526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a713eb7a363191895b52480a414f090d",
+    "url": "/LIM009-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "cc5a55eacda7451ee6e2",
+    "url": "/LIM009-fe-burger-queen/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "c60e4d98760f9dc4f277",
+    "url": "/LIM009-fe-burger-queen/static/css/main.42492b7c.chunk.css"
+  },
+  {
+    "revision": "cc5a55eacda7451ee6e2",
+    "url": "/LIM009-fe-burger-queen/static/js/2.2b6276c8.chunk.js"
+  },
+  {
+    "revision": "c60e4d98760f9dc4f277",
+    "url": "/LIM009-fe-burger-queen/static/js/main.18a19577.chunk.js"
+  },
+  {
+    "revision": "4ec68e84b8b177095eae",
+    "url": "/LIM009-fe-burger-queen/static/js/runtime~main.63907d42.js"
+  },
+  {
+    "revision": "7da93150c061111b03dd1d78e4f58aa8",
+    "url": "/LIM009-fe-burger-queen/static/media/burgerqueen.7da93150.png"
+  }
+]);
